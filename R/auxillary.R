afceff034d7d6ed61c4c7ab8bcde4f8e69a44f4b
SethOttoQuant <49544591+SethOttoQuant@users.noreply.github.com>
2021-10-21 09:07:51 -1000

    pretty_plot() work with unlabled data too

--- R/auxillary.R
+++ R/auxillary.R
@@ -10,12 +10,14 @@ pretty_plot <- function(X, x = NULL, lwd = 2, xlab = "Date", ylab = "", legend_p
     X <- X[ , !colnames(X)%in%"ref_date"]
   }else if(is.null(x)) x <- 1:NROW(X)
   k <- ncol(X)
-  color <- rep(c("black", "deeppink", "deepskyblue3", "green3", "tan3", "turquoise"),5)
+  color <- rep(c("black", "deeppink", "deepskyblue3", "green3", "tan3", "turquoise", "darkgrey", "steelblue", "violetred", "yellow3"),5)
   color <- color[1:k]
   matplot(x, X, type = 'l', lty = 1, col = color, lwd = lwd, xlab = xlab, ylab = ylab)
   grid(col = "lightslategrey")
-  legend(legend_pos, colnames(X),
-         col = color, lwd = lwd) #  bty = "n"
+  if(!is.null(colnames(X))){
+    legend(legend_pos, colnames(X),
+           col = color, lwd = lwd) #  bty = "n"
+  }
   title(title)
 }
 
